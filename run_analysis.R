766c8de58594802c31f704cb5710a3a9e32adc22
Kamil Ciemniewski <kamil@ciemniew.ski>
2019-02-26 08:36:28 +0000

    Added functions for readoing raw data and outputting as matrices and factor vectors

--- run_analysis.R
+++ run_analysis.R
@@ -36,6 +36,25 @@ download.raw.data <- function() {
   }
 }
 
-
+read.data <- function() {
+  cat(blue('Loading data into matrices and vectors\n'))
+  
+  train.x <- scan('data/train/X_train.txt') %>% matrix(ncol = 561, byrow = TRUE)
+  train.y.raw <- read_lines('data/train/y_train.txt')
+  test.x <- scan('data/test/X_test.txt')  %>% matrix(ncol = 561, byrow = TRUE)
+  test.y.raw <- read_lines('data/test/y_test.txt')
+  
+  y.levels <- c(train.y.raw, test.y.raw) %>% unique()
+  train.y <- factor(train.y.raw, levels=y.levels)
+  test.y <- factor(test.y.raw, levels=y.levels)
+  
+  list(
+    train.x = train.x,
+    test.x = test.x,
+    train.y = train.y,
+    test.y = test.y
+  )
+}
 
 download.raw.data()
+data <- read.data()
